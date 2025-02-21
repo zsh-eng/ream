744f685bed2eb3143c311dfe71d4408ad05d91a9
zsheng <zsheng.dev@gmail.com>
2025-02-20 21:38:49 -0500

    feat: implement search

--- entrypoints/saved/pages/articles.tsx
+++ entrypoints/saved/pages/articles.tsx
@@ -4,6 +4,7 @@ import { ReamDB } from '@/lib/db';
 import { cn } from '@/lib/utils';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { CalendarArrowDown, CalendarArrowUp, LinkIcon } from 'lucide-react';
+import MiniSearch from 'minisearch';
 import { Link } from 'wouter';
 
 export default function ArticlesPage() {
@@ -11,7 +12,7 @@ export default function ArticlesPage() {
     return ReamDB.articles.toArray();
   });
 
-  const [dateSort, setDateSort] = useState<'asc' | 'desc' | null>('desc');
+  const [dateSort, setDateSort] = useState<'asc' | 'desc'>('desc');
   const [search, setSearch] = useState('');
 
   const sortedArticles = useMemo(() => {
@@ -22,28 +23,57 @@ export default function ArticlesPage() {
     });
   }, [articles, dateSort]);
 
+  const miniSearch = new MiniSearch({
+    fields: ['title', 'url', 'siteName', 'excerpt'],
+    storeFields: ['title', 'url', 'siteName', 'excerpt'],
+  });
+
+  miniSearch.addAll(
+    articles?.map((article) => ({
+      ...article,
+      id: article.url,
+    })) ?? []
+  );
+  const searchResults = useMemo(
+    () => miniSearch.search(search, { prefix: true }),
+    [miniSearch, search]
+  );
+
+  const articlesToDisplay = search ? searchResults : sortedArticles;
+
   return (
     <>
-      <div className='w-full min-h-screen flex items-start bg-background py-16 animate-fadein'>
+      <div className='w-full min-h-screen flex items-start bg-background py-16 animate-fadein px-4'>
         <div className='w-0 lg:w-32 xl:w-48 h-full border-r-2 border-muted-foreground' />
-        <div className='flex flex-col gap-0'>
-          <div className='flex flex-col gap-0 sticky top-0 bg-background z-20 py-6 before:absolute before:content-[""] before:bg-background before:h-full before:w-[200px] before:right-full before:top-0 after:absolute after:content-[""] after:bg-background after:h-full after:w-[200px] after:left-full after:top-0'>
+        <div className='flex flex-col gap-0 max-w-2xl'>
+          <div
+            className={cn(
+              'flex flex-col gap-0 sticky top-0 bg-background z-20 py-6',
+              // Ensure that the stick covers the scaled up version of the article card
+              'before:absolute before:content-[""] before:bg-background before:h-full before:w-[200px] before:right-full before:top-0 after:absolute after:content-[""] after:bg-background after:h-full after:w-[200px] after:left-full after:top-0',
+              'w-[var(--container-2xl)]'
+            )}
+          >
             <div className='flex items-center gap-2'>
               <h1 className='text-4xl font-bold'>Saved Articles</h1>
               <div className='flex-1'></div>
 
-              <Button
-                variant='ghost'
-                size='icon'
-                className='size-10'
-                onClick={() => setDateSort(dateSort === 'asc' ? 'desc' : 'asc')}
-              >
-                {dateSort === 'asc' ? (
-                  <CalendarArrowUp className='size-6' />
-                ) : (
-                  <CalendarArrowDown className='size-6' />
-                )}
-              </Button>
+              {!search && (
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='size-10'
+                  onClick={() =>
+                    setDateSort(dateSort === 'asc' ? 'desc' : 'asc')
+                  }
+                >
+                  {dateSort === 'asc' ? (
+                    <CalendarArrowUp className='size-6' />
+                  ) : (
+                    <CalendarArrowDown className='size-6' />
+                  )}
+                </Button>
+              )}
             </div>
 
             <div className='mt-4'>
@@ -59,8 +89,8 @@ export default function ArticlesPage() {
             </div>
           </div>
 
-          <div className='max-w-2xl flex flex-col gap-1'>
-            {sortedArticles?.map((article) => (
+          <div className='flex flex-col gap-1'>
+            {articlesToDisplay?.map((article) => (
               <Link
                 key={article.url}
                 href={`/article/${encodeURIComponent(article.url)}`}
@@ -74,12 +104,9 @@ export default function ArticlesPage() {
                   <div className='text-xl'>{article.title}</div>
                   <div className='flex items-center gap-1 mb-2 mt-1'>
                     <LinkIcon className='size-3' />
-                    <a
-                      href={article.url}
-                      className='text-xs text-muted-foreground underline line-clamp-1 overflow-ellipsis'
-                    >
+                    <p className='text-xs text-muted-foreground underline line-clamp-1 overflow-ellipsis'>
                       {article.url}
-                    </a>
+                    </p>
                   </div>
                   <p className='line-clamp-3 overflow-ellipsis'>
                     {article.excerpt}
