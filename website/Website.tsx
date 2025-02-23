7cdb35734dc596fc1c21881fb96f9aea1065e275
zsheng <zsheng.dev@gmail.com>
2025-02-22 21:18:05 -0500

    fix: fix styling of website

--- website/Website.tsx
+++ website/Website.tsx
@@ -1,5 +1,7 @@
 import NavigationAndShorcutsContainer from '@/components/navigation-and-shortcuts';
+import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { ScrollText } from 'lucide-react';
 import { useTheme } from '~/hooks/use-theme';
 
 export default function Website() {
@@ -10,15 +12,20 @@ export default function Website() {
         <div className='w-0 lg:w-32 xl:w-48 h-full border-r-2 border-muted-foreground' />
         <article className={cn('prose', size)}>
           <h1 className='mb-0'>Ream</h1>
-          <p className='mt-4 mb-0'>
+          <p className=''>
             <span>Ream is a free, modern, </span>
-            <a href='https://github.com/zsh-eng/ream'>open-source</a>
+            <a href='https://github.com/zsh-eng/ream' target='_blank'>
+              open-source
+            </a>
             <span> reader mode extension.</span>
+            <br />
+            <span>
+              Ream simplifies the web by removing ads, sidebars, and unnecessary
+              formatting, leaving only the essential text and images for you to
+              read.
+            </span>
           </p>
-          <p className='mt-0'>
-            Ream simplifies the web for reading by removing ads, sidebars, and
-            unnecessary formatting, leaving only the essential text and images.
-          </p>
+          <p className=''></p>
           <p>
             <span>You can find Ream in the </span>
             <a
@@ -40,7 +47,17 @@ export default function Website() {
         </article>
       </div>
 
-      <NavigationAndShorcutsContainer />
+      <NavigationAndShorcutsContainer
+        renderActionButtons={() => (
+          <div className='flex items-center gap-2'>
+            <a href='https://github.com/zsh-eng/ream' target='_blank'>
+              <Button variant='ghost' size='icon'>
+                <ScrollText className='size-7' />
+              </Button>
+            </a>
+          </div>
+        )}
+      />
     </>
   );
 }
